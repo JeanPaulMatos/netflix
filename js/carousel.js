d65d8db6c20c9e8ce735f2ea3babb82a3f516bf3
jeanpaulmatos <mat9807jp@gmail.com>
2021-03-09 22:20:18 -0500

    Se añadierón las funcionalidades del Carrousel Desktop

--- js/carousel.js
+++ js/carousel.js
@@ -2,43 +2,352 @@ window.addEventListener('load', function () {
 
     new Glider (document.querySelector('.list__container--carousel'), {
 
-        slidesToShow: 3,            
-        slidesToScroll: 2,
-        // scrollLock: true,
+        slidesToShow: 3,
+        slidesToScroll: 3,
         draggable: true,
         dots: '.list__indicators',
         arrows: {
-        prev: '.list__container--previous',
-        next: '.list__container--after'
+            prev: '.list__container--previous',
+            next: '.list__container--after'
         },
-        
+
         responsive: [
+
             {
-            // screens greater than >= 775px
-            breakpoint: 768,
-            settings: {
-            // Set to `auto` and provide item width to adjust to viewport
-                slidesToShow: '5',
-                slidesToScroll: '5',
-                // itemWidth: 150,
-                // duration: 0.25
-                arrows: {
-                prev: '.list__container--previous',
-                next: '.list__container--after'
-                },
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
             }
-            },{
-            // screens greater than >= 1024px
-            // breakpoint: 1024,
-            // settings: {
-            //     slidesToShow: 2,
-            //     slidesToScroll: 1,
-            //     itemWidth: 150,
-            //     duration: 0.25
-            // }
-            }
+
         ]
 
-    });
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.continue__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.continue__indicators',
+        arrows: {
+            prev: '.continue__container--previous',
+            next: '.continue__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.originals__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.originals__indicators',
+        arrows: {
+            prev: '.originals__container--previous',
+            next: '.originals__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            },
+
+            {
+                breakpoint: 1550,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.tendencias__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.tendencias__indicators',
+        arrows: {
+            prev: '.tendencias__container--previous',
+            next: '.tendencias__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.worthy__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.worthy__indicators',
+        arrows: {
+            prev: '.worthy__container--previous',
+            next: '.worthy__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.dramas__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.dramas__indicators',
+        arrows: {
+            prev: '.dramas__container--previous',
+            next: '.dramas__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.youth__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.youth__indicators',
+        arrows: {
+            prev: '.youth__container--previous',
+            next: '.youth__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
+
+});
+
+window.addEventListener('load', function () {
+
+    new Glider (document.querySelector('.comedy__container--carousel'), {
+
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        draggable: true,
+        dots: '.comedy__indicators',
+        arrows: {
+            prev: '.comedy__container--previous',
+            next: '.comedy__container--after'
+        },
+
+        responsive: [
+
+            {
+                breakpoint: 767,
+                settings: {
+                    slidesToShow: '4',
+                    slidesToScroll: '4',
+                    draggable: false,
+                    rewind: true,
+                    duration: 0.6
+                    
+                }
+            }, 
+            
+            {
+                breakpoint: 1280,
+                settings: {
+                    slidesToShow: 4.5,
+                    slidesToScroll: 4,
+                    rewind: true,
+                    duration: 0.6
+                }
+            }
+
+        ]
+
+        });
 
 });
